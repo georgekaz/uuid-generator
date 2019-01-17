cb062fb285d2ff1a4a25b061126067fc7aff5e9d
George <egeorgekaz@gmail.com>
2019-01-17 07:59:22 +0000

    fix incorrect path from tutorial

--- brigade.js
+++ brigade.js
@@ -11,9 +11,8 @@ events.on("push", function(e, project) {
 
   // Now we want it to run these commands in order:
   node.tasks = [
-    "cd /src/app",
+    "cd /src",
     "pip install -r requirements.txt",
-    "cd /src/",
     "python setup.py test"
   ]
 
