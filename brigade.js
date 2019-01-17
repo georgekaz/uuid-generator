9cc4185f53a8dbd80c4b02c1b1d21f8068ad9c4c
George <egeorgekaz@gmail.com>
2019-01-17 09:16:34 +0000

    fixed syntax from badly rendered tutorial

--- brigade.js
+++ brigade.js
@@ -1,32 +1,32 @@
 const { events, Job } = require("brigadier");
 
 events.on("push", function(e, project) {
-  console.log("received push for commit " + e.revision.commit)
+	console.log("received push for commit " + e.revision.commit);
 
-  // Create a new job
-  var node = new Job("test-runner")
+	// Create a new job
+	var node = new Job("test-runner");
 
-  // We want our job to run the stock Docker Python 3 image
-  node.image = "python:3"
+	// We want our job to run the stock Docker Python 3 image
+	node.image = "python:3";
 
-  // Now we want it to run these commands in order:
-  node.tasks = [
-    "cd /src",
-    "pip install -r requirements.txt",
-    "python setup.py test"
-  ]
+	// Now we want it to run these commands in order:
+	node.tasks = [
+		"cd /src",
+		"pip install -r requirements.txt",
+		"python setup.py test"
+	];
 
-  // Display logs from the job Pod
-  node.streamLogs = true;
+	// Display logs from the job Pod
+	node.streamLogs = true;
 
-  // We're done configuring, so we run the job
-  node.run().then( () =&gt; {
-    events.emit("test-done", e, project)
-  })
+	// We're done configuring, so we run the job
+	node.run().then( () => {
+		events.emit("test-done", e, project)
+	});
 
-})
+});
 
-events.on("test-done", (e, project) =&gt; {
+events.on("test-done", (e, project) => {
 
 	var dockerBuild = new Job("docker-build");
 
@@ -46,9 +46,9 @@ events.on("test-done", (e, project) =&gt; {
 		"docker build -t quay.io/sohohouse/georgekaz-brigade-test:latest ." ,
 		"docker login -u $DOCKER_USER -p $DOCKER_PASS",
 		"docker push quay.io/sohohouse/georgekaz-brigade-test:latest"
-	]
+	];
 
-	dockerBuild.run().then( () =&gt; {
+	dockerBuild.run().then( () => {
 		events.emit("build-done", e, project);
-	})
-})
+	});
+});
